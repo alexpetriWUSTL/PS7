6646993ff11eacf7bf9f72be4724c1233c888edd
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-04-22 16:14:59 -0500

    Completed 3 and 4
    Developed solutions for problems 3 and 4 - used filter to sort out all strings with robbery in them via str_detect in stringr

--- PS7_code.R
+++ PS7_code.R
@@ -7,13 +7,30 @@ crimeData <- read.csv("March2018.csv")
 crimeData <- as_tibble(crimeData)
 
 
-
+#2
 crimeType_Day <- crimeData %>% 
   mutate(DateOccur, day = sapply(as.character(DateOccur), substr, start = 1, stop = 10)) %>%  #clean up date data - we don't want time just the day
   mutate(Description, crime = sapply(as.character(crimeData$Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
     strsplit(x = x, split = "[-/]")[[1]][1] #[[1]][1] because the data type for strsplit is a list of observations
   })) %>% 
   group_by(crime, day) %>% #group the new tibble by the cleaned data crime and day
-  summarise(count =n()) #summarize the count
+  summarise(count = n()) #summarize the count
 
 class(crimeType_Day)
+
+
+#3 - Neighborhood no. 35 has 305 crimes, the highest number
+crimeType_Neighborhood <- crimeData %>%
+  group_by(Neighborhood) %>% #use the pipes to group it down to neighborhood
+  summarise(count = n()) #summarise the number of times that neighborhood appears
+
+arrange(crimeType_Neighborhood, desc(count)) #sort in descending order to find the highest crime neighborhood
+
+#4
+crimeType_Robbery <- crimeData %>%
+  filter(str_detect(Description, paste("ROBBERY", collapse="|"))) %>%
+  group_by(District) %>%
+  summarise(count = n())
+
+
+
