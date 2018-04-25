9675e91442b4d2f2aad8e73bc8372a6120166974
alexpetriWUSTL <36051450+alexpetriWUSTL@users.noreply.github.com>
2018-04-24 21:58:40 -0500

    Completed graphs
    The graphs #5 and 6 completed. If no other updates, this is my final code for the problem set.

--- PS7_code.R
+++ PS7_code.R
@@ -9,7 +9,7 @@ crimeData <- as_tibble(crimeData)
 #2
 crimeType_Day <- crimeData %>% #cleaned and sorted type of crime per day
   mutate(DateOccur, day = sapply(as.character(DateOccur), substr, start = 1, stop = 10)) %>%  #clean up date data - we don't want time just the day
-  mutate(Description, crime = sapply(as.character(crimeData$Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
+  mutate(Description, crime = sapply(as.character(Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
     strsplit(x = x, split = "[-/]")[[1]][1] #[[1]][1] because the data type for strsplit is a list of observations
   })) %>% 
   group_by(crime, day) %>% #group the new tibble by the cleaned data crime and day
@@ -18,6 +18,8 @@ crimeType_Day <- crimeData %>% #cleaned and sorted type of crime per day
 #the question asks us to first sort the number of crime per day by type of crime
 #and then find the most common crimes in march. As a result, I will compute something for crime totals in march
 #as well
+
+
 crime_March <- crimeData %>%
   mutate(DateOccur, day = sapply(as.character(DateOccur), substr, start = 1, stop = 10)) %>%  #clean up date data - we don't want time just the day
   mutate(Description, crime = sapply(as.character(crimeData$Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
@@ -32,8 +34,9 @@ arrange(crime_March, desc(count)) #Larceny, Leaving the scene, and Destruction o
 #3 - Neighborhood no. 35 has 305 crimes, the highest number
 crimeType_Neighborhood <- crimeData %>%
   group_by(Neighborhood) %>% #use the pipes to group it down to neighborhood
-  summarise(count = n()) %>% #summarise the number of times that neighborhood appears
-  arrange(crimeType_Neighborhood, desc(count)) #sort in descending order to find the highest crime neighborhood
+  summarise(count = n()) #summarise the number of times that neighborhood appears
+  
+arrange(crimeType_Neighborhood, desc(count)) #sort in descending order to find the highest crime neighborhood
 
 
 #4 District 5 has the highest robbery proportion
@@ -55,71 +58,50 @@ district_Proportion <- crimeType_All %>%
 arrange(district_Proportion, desc(proportion)) #sort in descending order to find the highest crime neighborhood
 
 
-#5
+#5 For my types of crime, I use the 5 most common crimes from question #2
 library(ggplot2)
+library(tidyr)
+
+crimeType_Day_March <- crimeType_Day %>% #make sure the dates are in march because there are some non-march dates that are likely crimes reported in march that took place earlier
+  filter(substr(day, start = 1, stop = 2) == "03" & substr(day, start = 7, stop = 10) == "2018")
   
-crimeType <- crimeData %>%
-  mutate(DateOccur, day = sapply(as.character(DateOccur), substr, start = 1, stop = 10)) %>%  #clean up date data - we don't want time just the day
-  mutate(Description, crime = sapply(as.character(crimeData$Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
-    strsplit(x = x, split = "[-/]")[[1]][1] #[[1]][1] because the data type for strsplit is a list of observations
-  })) %>%
-  mutate(Crime, category = sapply(Crime, function(x){ #each crime is given a category, and after exploring the data i noticed each group of crimes has a unique code by 10000s (ex homicide 10000, robbery 20000)
-    if(nchar(as.character(x)) == 5){
-      substr(paste("0", as.character(x), sep = ""), start = 1, stop = 2) #need to take a substring, but must have the code be 01 to take a substring
-    } else {
-      substr(as.character(x), start = 1, stop = 2)
-    }
-    })
-  ) %>%
-  select(District, Neighborhood, day, crime, category)  #really only need these variables 
-  
-summaryType <- crimeType %>% 
-  group_by(crime) %>% #group by crime to visualize
-  summarise(count = n(), category = mean(as.numeric(category))) #call summarise to visualize what category each crime is in
-  
-print(arrange(summaryType, desc(category)), n = 35)
+topFive <- crimeType_Day_March %>% #filter out 5 most prominent crimes
+  filter(crime == "LARCENY" | crime == "LEAVING SCENE OF ACCIDENT" | crime == "DESTRUCTION OF PROPERTY"
+         | crime == "AGG.ASSAULT" | crime == "AUTO THEFT") %>%
+  mutate(day, day = substr(day, start = 2, stop = 5)) #take substring of date to make it shorter and able to fit on an axis
 
-#now I want to recode what I developed above. I want to group all thefts together, all violent crimes, and all miscellaneous crimes
-violentPicture <- crimeType %>%
-  filter(as.numeric(category) == 1 |
-                         as.numeric(category) == 17 |
-                         as.numeric(category) == 14 |
-                         as.numeric(category) == 9 |
-                         as.numeric(category) == 4 |
-                         as.numeric(category) == 2) %>%
-  group_by(day, crime) %>%
-  summarise(count = n()) %>%
-  filter(substr(day, start = 1, stop = 2) == "03")
-
-drugs_alchohol_Picture <- filter(as.numeric(category) == 26 |
-                         as.numeric(category) == 25 |
-                         as.numeric(category) == 24 |
-                         as.numeric(category) == 22 |
-                         as.numeric(category) == 21 |
-                         as.numeric(category) == 20 |
-                         as.numeric(category) == 18 |
-                         as.numeric(category) == 15 |
-                         as.numeric(category) == 8 = "misc")
-
-theftPicture <- filter
-
-
-
-ggplot(data=violentPicture, mapping = aes(x = day, y = count, col = crime)) +
+ggplot(topFive, aes(x = day, y = count, colour = crime, group = crime)) + #plot it with appropriate labels
   geom_line() +
-  geom_point()
+  xlab("Day in March") +
+  ylab("Crime Occurences") +
+  ggtitle("Crime Occurences for the 5 Most Common STL Crimes in March") +
+  theme(axis.text.x = element_text(vjust = 0)) +
+  scale_color_manual(values = c("blue", "red", "green",  
+                                "orange", "purple"), labels = c("Aggravated Assault", "Auto Theft",
+                                "Destruction of Property", "Larceny", "Leaving the Scene of an Accident"))
+
 
 #6
-cleaned <- crimeData %>%
-  mutate(DateOccur, day = sapply(as.character(DateOccur), substr, start = 1, stop = 10)) %>%  #clean up date data - we don't want time just the day
-  mutate(Description, crime = sapply(as.character(crimeData$Description), function(x){ #clean up the description data - make the crimes more generalizable (robbery without specifying carjacking)
-    strsplit(x = x, split = "[-/]")[[1]][1] #[[1]][1] because the data type for strsplit is a list of observations
-  })) 
+larceny <- crimeData %>% #filter out larceny
+  mutate(District, District = as.character(District)) %>% #District needs to be a character so it is a discrete variable for graphing
+  filter(str_detect(Description, "LARCENY")) %>% 
+  filter(substr(DateOccur, start = 1, stop = 2) == "03" & substr(DateOccur, start = 7, stop = 10) == "2018" & District != 0) %>% #keep only March 2018 days
+  mutate(DateOccur, day = substr(DateOccur, start = 2, stop = 5)) %>% #take substring of date to make it shorter and able to fit on an axis
+  group_by(District, day) %>%
+  summarise(count = n()) 
 
-districtOne <- crimeData %>% 
-  filter(District == 1) 
+ggplot(larceny, aes(x = day, y = count, colour = District, group = District)) + #plot it with appropriate labels
+  geom_line() +
+  xlab("Day in March") +
+  ylab("Larceny Occurences") +
+  ggtitle("Larceny Occurences by STL District in March") +
+  scale_color_manual(values = c("lightblue", "red", "green",  
+                               "orange", "purple", "black"), labels = c("1", "2", '3', '4', '5', '6'))
 
 
 
-ggplot(data = districtOne) +
-  geom_line(aes(x = day, y = ))
+
+
+
+
+
